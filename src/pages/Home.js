4b27b88538f84286e5ec090e34eca777abd353f0
piyush-ripo <nikhilchahar118@gmail.com>
2024-11-08 17:07:00 +0530

    submit

---
+++ src/pages/Home.js
@@ -0,0 +1,55 @@
+import React, { useEffect, useState } from 'react'
+import { useNavigate } from 'react-router-dom';
+import { handleError, handleSuccess } from '../utils';
+import { ToastContainer } from 'react-toastify';
+import App from './App';
+function Home() {
+    const [loggedInUser, setLoggedInUser] = useState('');
+    const [products, setProducts] = useState('');
+    const navigate = useNavigate();
+    useEffect(() => {
+        setLoggedInUser(localStorage.getItem('loggedInUser'))
+    }, [])
+
+    const handleLogout = (e) => {
+        localStorage.removeItem('token');
+        localStorage.removeItem('loggedInUser');
+        handleSuccess('User Loggedout');
+        setTimeout(() => {
+            navigate('/login');
+        }, 1000)
+    }
+
+    const fetchProducts = async () => {
+        try {
+            const url = "http://localhost:8080/products";
+            const headers = {
+                headers: {
+                    'Authorization': localStorage.getItem('token')
+                }
+            }
+            const response = await fetch(url, headers);
+            const result = await response.json();
+            console.log(result);
+            setProducts(result);
+        } catch (err) {
+            handleError(err);
+        }
+    }
+    useEffect(() => {
+        fetchProducts()
+    }, [])
+
+    return (
+        <div>
+            <h1>Welcome {loggedInUser}</h1>
+            <button onClick={handleLogout}>Logout</button>
+            <div>
+            <App />
+            </div>
+            <ToastContainer />
+        </div>
+    )
+}
+
+export default Home
